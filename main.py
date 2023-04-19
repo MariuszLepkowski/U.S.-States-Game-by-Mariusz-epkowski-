24f559c43030d71e4b6060725e3a2f10e2e1d905
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-04-19 17:09:43 +0200

    Line 20 - for loop replaced by list comprehension

--- main.py
+++ main.py
@@ -13,14 +13,11 @@ list_of_states = states.state.to_list()
 
 score = 0
 correct_guesses = []
-states_to_learn = []
 
 while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f"{score}/50 Guess the State", prompt="What's another state name?").title()
     if answer_state == "Exit":
-        for state in list_of_states:
-            if state not in correct_guesses:
-                states_to_learn.append(state)
+        states_to_learn = [state for state in list_of_states if state not in correct_guesses]
         data = pandas.DataFrame(states_to_learn)
         data.to_csv("states_to_learn.csv")
         break
